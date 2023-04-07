c298c235dfcb4ecc8e6c9419ec300398a3db353f
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-07 10:32:23 +0300

    implemented register view

--- app.py
+++ app.py
@@ -114,7 +114,49 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        
+        # Ensure password and confirmation password are the same
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return apology("passwords don't match", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username doesn't exist already
+        if len(rows) > 0:
+            return apology("username already exists", 403)
+        
+        # Add new user to database
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
+                   request.form.get("username"),
+                   generate_password_hash(request.form.get("password")))
+        
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
